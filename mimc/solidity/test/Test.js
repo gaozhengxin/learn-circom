61f2820fabffda5cfb675c8b2e527a815c61b488
gaozhengxin <zhengxingao@live.cn>
2022-12-20 18:18:41 +0800

    update test

--- mimc/solidity/test/Test.js
+++ mimc/solidity/test/Test.js
@@ -12,5 +12,6 @@ describe("Test MiMC", function () {
 
     let hash = await hasher.hash([1000, 2000], { gasLimit: 10000000000 })
     console.log(`hash : ${hash}`)
+    expect(hash).to.equal(6213744475698949925836804247423411578377719488321982497116899109773276757104n)
   });
 });
